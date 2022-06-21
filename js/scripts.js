fd0e2463b88cdf92f5b7d9bce68ab0e210ca1bbb
Shelby Trodden <shelby.trodden@gmail.com>
2022-06-20 19:01:54 -0500

    clean up script typos

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 // User Interface Logic
 window.addEventListener("load", function() {
     let quiz = document.getElementById("quiz");
-    
     let resetBtn = document.getElementById("reset");
-  
     let form = document.querySelector("form");
+    
   
-    form.addEventListener("submit", function(event) {
-      const qOneInput = document.getElementById(parseInt("#question1Input").value());
-      const qTwoInput = document.getElementById(parseInt("#question2Input").value());
-      const qThreeInput= document.getElementById(parseInt("#question3Input").value());
-      const qFourInput = document.getElementById(parseInt("#question4Input").value());
-      const qFiveInput = document.getElementById(parseInt("#question5Input").value());
-      const qSixInput = document.getElementById(parseInt("#question6Input").value());
+    form.addEventListener("submit", function() {
+
+      const qOneInput = document.getElementById(parseInt("question1").value());
+      const qTwoInput = document.getElementById(parseInt("question2").value());
+      const qThreeInput= document.getElementById(parseInt("question3").value());
+      const qFourInput = document.getElementById(parseInt("question4").value());
+      const qFiveInput = document.getElementById(parseInt("question5").value());
+      const qSixInput = document.getElementById(parseInt("question6").value());
 
       let final = qOneInput+qTwoInput+qThreeInput+qFourInput+qFiveInput+qSixInput
         if (final <6){
@@ -24,9 +24,6 @@ window.addEventListener("load", function() {
         } else {
           ("#resultC").show();  
         }
-
-      event.preventDefault();
-
     });
     
     form.addEventListener("submit", function() {
@@ -34,11 +31,11 @@ window.addEventListener("load", function() {
     }); 
   
     resetBtn.addEventListener("click", function() {
-      document.getElementById("question1Input").value = null;
-      document.getElementById("question2Input").value = null;
-      document.getElementById("question3Input").value = null;
-      document.getElementById("question4Input").value = null;
-      document.getElementById("question5Input").value = null;
-      document.getElementById("question6Input").value = null;
+      document.getElementById("question1").value = null;
+      document.getElementById("question2").value = null;
+      document.getElementById("question3").value = null;
+      document.getElementById("question4").value = null;
+      document.getElementById("question5").value = null;
+      document.getElementById("question6").value = null;
     });
   });

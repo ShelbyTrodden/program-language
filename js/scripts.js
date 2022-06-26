31bcef74c177c24dd9ba9ad90955db07ce8271f2
Shelby Trodden <shelby.trodden@gmail.com>
2022-06-26 09:38:03 -0500

    moving variables inside submit function

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,34 @@
 // User Interface Logic
-window.addEventListener("load", function() {
+window.addEventListener("load", function(event) {
+  event.preventDefault();
 
-  let form = document.querySelector("form"); 
-  let quiz = document.getElementById("quiz");
-  let resetBtn = document.getElementById("reset");
+  const form = document.getElementById("form"); 
+  const results = document.getElementById("results");
+  const input = document.getElementById("input");
+  const resetBtn = document.getElementById("reset");
    
-    
-  const qOneInput = parseInt(document.getElementById("question1").value);
-  const qTwoInput = parseInt(document.getElementById("question2").value);
-  const qThreeInput= parseInt(document.getElementById("question3").value);
-  const qFourInput = parseInt(document.getElementById("question4").value);
-  const qFiveInput = parseInt(document.getElementById("question5").value);
-  const qSixInput = parseInt(document.getElementById("question6").value);
   
 form.addEventListener("submit", function() {
+  event.preventDefault();
 
-  let final = qOneInput + qTwoInput + qThreeInput + qFourInput + qFiveInput + qSixInput
-    if (final <6) {
+  const qOneInput = parseInt(document.getElementById("#question1").value);
+  const qTwoInput = parseInt(document.getElementById("#question2").value);
+  const qThreeInput= parseInt(document.getElementById("#question3").value);
+  const qFourInput = parseInt(document.getElementById("#question4").value);
+  const qFiveInput = parseInt(document.getElementById("#question5").value);
+  const qSixInput = parseInt(document.getElementById("#question6").value);
+
+  const final = qOneInput + qTwoInput + qThreeInput + qFourInput + qFiveInput + qSixInput
+    if (final < 6) {
       document.getElementById("resultNull").classList.toggle("visible");
-    } else if (final <9){
+    } else if (final < 9){
       document.getElementById("resultA").classList.toggle("visible");
-    } else if  (final <13 ){
+    } else if  (final < 13 ){
       document.getElementById("resultB").classList.toggle("visible");
     } else {
       document.getElementById("resultC").classList.toggle("visible");  
     }
+
   });
   
 resetBtn.addEventListener("click", function() {
@@ -34,5 +38,7 @@ resetBtn.addEventListener("click", function() {
       qFourInput.value = null;
       qFiveInput.value = null;
       qSixInput.value = null;
+      results.value = null;
+ 
     });
   });
